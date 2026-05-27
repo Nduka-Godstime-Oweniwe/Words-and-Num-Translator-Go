33cb1b80a70cd74ef80e18339ed1cd85ab7909dd
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-05-27 07:35:15 +0200

    just started the mainflow. Waiting for other functions to be completed before proceeding

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	// user := "three hundred thousand seven hundred and fifty four"
+	// user = strings.ToLower(user)
+	// slice := strings.Fields(user)
+	// // fmt.Println(slice)
+	// ints := translator.SliceOfInt(slice)
+	// // fmt.Println(ints)
+	// _, str := translator.TranslateToInt(ints)
+	// fmt.Println(numb)
+	// fmt.Println(str)
+	fmt.Println("Words and Numbers Translator")
+	fmt.Println("1. Convert Words to Numbers")
+	fmt.Println("2. Convert Numbers to words")
+	fmt.Print("Select An Option: ")
 
 }
