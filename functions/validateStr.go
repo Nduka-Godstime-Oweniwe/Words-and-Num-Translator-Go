7aa97286c13eae5b643be23d83b6cf5364346430
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-06-04 09:16:22 +0100

    finished validating user number in words

--- functions/validateStr.go
+++ functions/validateStr.go
@@ -1,1 +1,20 @@
 package translator
+
+import "strings"
+
+func ValidStr(str string) bool {
+	if str == "" {
+		return false
+	}
+	slice := strings.Fields(str)
+	for i := 0; i < len(slice); i++ {
+		if !IsValidWord(slice[i]) {
+			return false
+		}
+	}
+
+	if slice[0] == "trillion" || slice[0] == "billion" || slice[0] == "million" {
+		return false
+	}
+	return true
+}
