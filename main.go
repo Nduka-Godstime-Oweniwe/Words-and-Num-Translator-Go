a807bdf1ab905bb4b79751a1742fcdff07d44d7d
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-06-04 09:36:18 +0100

    worked on the main flow

--- main.go
+++ main.go
@@ -2,21 +2,64 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	translator "translator/functions"
 )
 
-func main() {
-	// user := "three hundred thousand seven hundred and fifty four"
-	// user = strings.ToLower(user)
-	// slice := strings.Fields(user)
-	// // fmt.Println(slice)
-	// ints := translator.SliceOfInt(slice)
-	// // fmt.Println(ints)
-	// _, str := translator.TranslateToInt(ints)
-	// fmt.Println(numb)
-	// fmt.Println(str)
-	fmt.Println("Words and Numbers Translator")
-	fmt.Println("1. Convert Words to Numbers")
-	fmt.Println("2. Convert Numbers to words")
-	fmt.Print("Select An Option: ")
+func clearScreen() {
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
 
+func main() {
+	for {
+		clearScreen()
+		fmt.Println("1. Translate Numbers to Words")
+		fmt.Println("2. Translate Words To Numbers")
+		fmt.Println("3. Exit")
+		option := translator.UserOption("Select An Option: ", 3)
+		clearScreen()
+		if option == 1 {
+			for {
+
+				num := translator.UserInput("Enter Number: ")
+				number, valid := translator.ValidateInt(num)
+				if !valid {
+					fmt.Println("Invalid Number")
+					continue
+				}
+				fmt.Println(translator.TranslateToWords(number))
+				translator.UserInput("Press Enter To continue: ")
+				break
+			}
+		} else if option == 2 {
+			for {
+				str := translator.UserInput("Enter number in words: ")
+				valid := translator.ValidStr(str)
+				if !valid {
+					fmt.Println("Invalid Number in words")
+					continue
+				}
+				num := translator.TranslateToInt(str)
+				numWithComma := translator.PrintNumberWithComma(num)
+				fmt.Println(numWithComma)
+				translator.UserInput("Press Enter To continue: ")
+				break
+			}
+
+		} else {
+			break
+		}
+
+	}
 }
