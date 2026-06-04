d70bf720181def02e481f003af90611c57560627
Nduka Oweniwe <oweniwenduka@gmail.com>
2026-06-04 09:03:37 +0100

    finished the userinput functions

--- functions/userInput.go
+++ functions/userInput.go
@@ -1,1 +1,29 @@
 package translator
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func UserInput(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(prompt)
+	result, _ := reader.ReadString('\n')
+	return strings.TrimSpace(result)
+}
+
+func UserOption(prompt string, limit int) int {
+	for {
+		value, err := strconv.Atoi(UserInput(prompt))
+
+		if err != nil || !(value >= 1 && value <= limit) {
+			fmt.Println("Invalid Option!")
+			continue
+		}
+		return value
+	}
+
+}
